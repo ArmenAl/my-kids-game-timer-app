097fbafee7fc24e2e5c2f3a5a5334c7860aaa8d4
ArmenAl <armen.al@gmail.com>
2020-09-09 23:37:33 +0200

    camera test

--- src/Components/TakePhotoPage.js
+++ src/Components/TakePhotoPage.js
@@ -1,19 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import Camera from "react-html5-camera-photo";
 import "react-html5-camera-photo/build/css/index.css";
 
-import "./TakePhotoPage.css";
+import ImagePreview from "./ImagePreview/ImagePreview";
 
 function TakePhotoPage(props) {
+  const [dataUri, setDataUri] = useState("");
+
   function TakePhoto(dataUri) {
     console.log("PhotoOK");
+    setDataUri(dataUri);
   }
+
+  const isFullscreen = false;
+
   return (
-    <Camera
-      onTakePhoto={(dataUri) => {
-        TakePhoto(dataUri);
-      }}
-    />
+    <div>
+      {dataUri ? (
+        <ImagePreview dataUri={dataUri} isFullscreen={isFullscreen} />
+      ) : (
+        <Camera
+          onTakePhotoAnimationDone={TakePhoto}
+          isFullscreen={isFullscreen}
+        />
+      )}
+    </div>
   );
 }
 export default TakePhotoPage;
